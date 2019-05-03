f289663fd66cb9204d4fce6cfa564aeed30ad6de
Matteo Pagin <mattpagg@gmail.com>
2019-05-03 14:46:45 +0200

    K-Means++ initial work

--- HW3_files/HW3.py
+++ HW3_files/HW3.py
@@ -1,6 +1,7 @@
 # Import needed linear algebra libraries
 from pyspark.mllib.linalg import Vectors # Functions
-from numpy import random as rn
+import numpy as np
+from numpy import random as rn, linalg as lin
 
 
 ## Needed Functions ##
@@ -14,39 +15,57 @@ def readVectorsSeq(filename):
     return vector_list
 
 # Sequential KMeans++ function
-def kmeansPP(P,WP,k,iter):
+def kmeansPP(P,WP,k,iter): # WP must be column vector
 
 	# Set of centers which will be returned on output
 	setCenters = []
-	# Each iter we need distance points to closest center
-	setMinDist = np.full((len(P)), np.inf) # Initially distances all +inf
 
 	# Start by picking first center at random
-	randChoice = rn.randint(1, len(P) + 1)
+	randChoice = rn.randint(0, len(P))
 	setCenters.append(P[randChoice])
-	# Remove such point from initial set
+	# Remove such point from initial set and weights set
 	P.pop(randChoice)
+	WP =  np.delete(WP, randChoice)
+	# Each iter we need distance points to closest center
+	setMinDist = np.zeros(len(P))
+
+	# Initial distance computation
+	for index in range(len(P)):
+		# Compute new distance, between point and new center
+		newDist = lin.norm(P[index] - setCenters[0])
+		setMinDist[index] = newDist
 
 	# Main algorithm loop
-	for round in range(1, k-1): # Only k-1 centers still have to be chosen
+	for round in range(0, k-1): # Only k-1 centers stil have to be chosen
 
-		# Each iter we need distance points to closest center
-		# keep track of the min one, update it only if such value decreases
-		for index in range(1, len(P)):
+		# Initialize probabilities bucket
+		setProb = np.zeros(len(P))
+		# Compute the points' probabilities
+		setProb = np.multiply(setMinDist, WP)
+		# Normalization factor
+		setProb = setProb/np.dot(setMinDist, WP) # Possible errors, in case specify first row
+
+		# Sample random int, use cumsum of pr.ties as tresholds
+		randChoice = rn.rand()
+		cumProb = np.cumsum(setProb)
+
+		# Check which center to choose, according to tresholds
+		luckyIndex = np.argwhere(cumProb > randChoice)[0]
+		luckyIndex = luckyIndex[0]
+		# Add winner to the centers set
+		print("luckyIndex " + str(luckyIndex))
+		setCenters.append(P[luckyIndex])
+		# Remove it from other sets
+		P.pop(luckyIndex)
+		WP = np.delete(WP, luckyIndex)
+
+		# Update min distance by checking if closer to new center than before
+		for index in range(len(P)):
 			# Compute new distance, between point and new center
-			newDist = np.norm(P[index] - setCenters[round])
+			newDist = lin.norm(P[index] - setCenters[round])
 			# Update distance if smaller
 			if(newDist < setMinDist[index]):
 				setMinDist[index] = newDist
-
-
-		# Initialize probabilities bucket
-		setProb = np.zeros(len(P), 1)
-		# Compute the points' probabilities
-		setProb = 
-		# Normalization factor
-
-
 
 
 	return setCenters
@@ -58,5 +77,14 @@ filename = "covtype10K.data"
 vectorData = readVectorsSeq(filename)
 # Now vectorData is a list of linalg.Vectors
 
+# Just testing set of 10 points in R2
+testWeights = np.ones(10)
+testPoints = []
+for k in range(10):
+	# Create point
+	testVector = [rn.uniform(), rn.uniform()]
+	testPoints.append(Vectors.dense(testVector))
 
-# test = kmeansPP(vectorData, np.)
+testOutput = kmeansPP(testPoints, testWeights, 3, 3)
+
+
