8117bd394e86ffce473f63d02305bb1d81b24110
gajdusep <gajdusek.pavel@gmail.com>
2019-05-08 14:13:11 +0200

    added lloyd algo

--- HW3_files/HW3.py
+++ HW3_files/HW3.py
@@ -1,10 +1,9 @@
-from pyspark.mllib.linalg import Vectors  # Functions
+from pyspark.mllib.linalg import Vectors
 import numpy as np
 from numpy import random as rn, linalg as lin
-import time  # Performance tests
-
-
-# Needed Functions #
+import time
+from functools import reduce
+from operator import add
 
 
 # Function given as a starting point, used to load file
@@ -16,63 +15,99 @@ def readVectorsSeq(filename):
 	return vector_list
 
 
+# lloyd function
+# takes too much time..?
+# points: list of points, centers: precomputed centers, iters: number of iterations in lloyd algo
+def lloyd(points, centers, iters):
+	points_length = len(points)
+	center_length = len(centers)
+
+	new_centers = centers.copy()
+	clustered_points = np.zeros(points_length)
+
+	for i in range(iters):
+		# compute the centers belonging to the points
+		for point_index in range(points_length):
+			point_cluster_index = -1
+			point_min_dist = np.inf
+			for center_index in range(center_length):
+				curr_dist = lin.norm(points[point_index] - new_centers[center_index])
+				if curr_dist < point_min_dist:
+					point_cluster_index = center_index
+					point_min_dist = curr_dist
+			clustered_points[point_index] = point_cluster_index
+
+		# recompute centers
+		for center_index in range(center_length):
+			curr_cluster_bool = clustered_points == center_index
+			curr_cluster_points = [points[i] for i in range(points_length) if curr_cluster_bool[i]]
+
+			vectors_sum = reduce(add, curr_cluster_points)
+			new_center = vectors_sum / len(curr_cluster_points)
+			new_centers[center_index] = new_center
+
+	return new_centers
+
+
 # Sequential KMeans++ function
-def kmeansPP(P, WP, k, iter):  # WP must be column vector
+def kmeansPP(points, point_weights, k, iter):  # point_weights must be column vector
 
 	# Set of centers which will be returned on output
 	setCenters = []
 
 	# Start by picking first center at random
-	randChoice = rn.randint(0, len(P))
-	setCenters.append(P[randChoice])
+	rand_choice = rn.randint(0, len(points))
+	setCenters.append(points[rand_choice])
 
 	# Remove such point from initial set and weights set
-	P.pop(randChoice)
-	WP = np.delete(WP, randChoice)
+	points.pop(rand_choice)
+	point_weights = np.delete(point_weights, rand_choice)
 
 	# Each iteration we need distance points to closest center
-	setMinDist = np.zeros(len(P))
+	setMinDist = np.zeros(len(points))
 
 	# Initial distance computation
-	for index in range(len(P)):
+	for index in range(len(points)):
 		# Compute new distance, between point and new center
-		setMinDist[index] = lin.norm(P[index] - setCenters[0])
+		setMinDist[index] = lin.norm(points[index] - setCenters[0])
 
 	# Main algorithm loop
 	for round in range(0, k-1):  # Only k-1 centers stil have to be chosen
 
 		# Compute the points' probabilities
-		setProb = np.multiply(setMinDist, WP)
+		setProb = np.multiply(setMinDist, point_weights)
 		# Normalization factor
-		setProb = setProb/np.dot(setMinDist, WP)  # Possible errors, in case specify first row
+		setProb = setProb/np.dot(setMinDist, point_weights)  # Possible errors, in case specify first row
 
 		# Sample random int, use cumsum of pr.ties as tresholds
-		randChoice = rn.rand()
+		rand_choice = rn.rand()
 		cumProb = np.cumsum(setProb)
 
 		# Check which center to choose, according to tresholds
-		luckyIndex = np.argwhere(cumProb > randChoice)[0]
+		luckyIndex = np.argwhere(cumProb > rand_choice)[0]
 		luckyIndex = luckyIndex[0]
 		# Add winner to the centers set
-		setCenters.append(P[luckyIndex])
+		setCenters.append(points[luckyIndex])
 		# Remove it from other sets
-		P.pop(luckyIndex)
-		WP = np.delete(WP, luckyIndex)
+		points.pop(luckyIndex)
+		point_weights = np.delete(point_weights, luckyIndex)
 		setMinDist = np.delete(setMinDist, luckyIndex)
 
 		# Update min distance by checking if closer to new center than before
-		for index in range(len(P)):
+		for index in range(len(points)):
 			# Compute new distance, between point and new center
-			newDist = lin.norm(P[index] - setCenters[round])
+			newDist = lin.norm(points[index] - setCenters[round])
 			# Update distance if smaller
-			if(newDist < setMinDist[index]):
+			if newDist < setMinDist[index]:
 				setMinDist[index] = newDist
 
+	start = time.time()
+	new_centers = lloyd(points, setCenters, iter)
+	end = time.time()
+	print("time in lloyd", end - start)
 
-	# Refine centers set using Lloyd's algorithm
-	### STILL TO DO ###
-
-	return setCenters
+	# for now: return precomputed and after lloyd to see the difference
+	return setCenters, new_centers
 
 
 # KMedian cost function, P set points, C set centers
@@ -106,6 +141,8 @@ filename = "covtype10K.data"
 vectorData = readVectorsSeq(filename)
 # Now vectorData is a list of linalg.Vectors
 
+# I think this test is kind of useless, it was useful just in the beginning, right?
+"""
 #####################################
 # Just testing set of x points in R2
 x = 20000
@@ -116,23 +153,27 @@ for k in range(x):
 	testVector = [rn.uniform(), rn.uniform()]
 	testPoints.append(Vectors.dense(testVector))
 
-testOutput = kmeansPP(testPoints, testWeights, 3, 3)
+testOutput, testOutputeNewOne = kmeansPP(testPoints, testWeights, 3, 3)
 #####################################
-
+"""
 
 # Test on the proper dataset 
 k = 5
 iterations = 10
-testOutput = kmeansPP(vectorData, np.ones(len(vectorData)), k, iterations)
+testOutput, test_new_output = kmeansPP(vectorData, np.ones(len(vectorData)), k, iterations)
 testCost = kmeansObj(vectorData, testOutput)
+test_new_cost = kmeansObj(vectorData, test_new_output)
 print("Average distance of a point from set of centers: " + str(testCost))
+print("Average distance of a point from set of centers: " + str(test_new_cost))
 
 # Check if it works: is it better than randomly picking the points as centers?
 # WARNING: Not particularly relevant without refinement, probably
 
+
 suspiciousTrials = 0
 maxTrials = 100
 
+# I don't really understand this :-o
 for trials in range(maxTrials):
 	# Pick k random centers, compute distance
 	randCenters = []
@@ -143,3 +184,4 @@ for trials in range(maxTrials):
 		suspiciousTrials = suspiciousTrials + 1
 
 print("We should be " + str(suspiciousTrials/maxTrials*100) + "% worried!")
+
