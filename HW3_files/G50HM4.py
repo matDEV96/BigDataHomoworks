7397b8024c76f61211318d736351e7ffb70f8699
gajdusep <gajdusek.pavel@gmail.com>
2019-06-10 18:26:09 +0200

    edits

--- HW3_files/G50HM4.py
+++ HW3_files/G50HM4.py
@@ -13,40 +13,8 @@ from operator import add
 
 
 # --------------------------------------------------------------
+# ------- our methods ------------------------------------------
 # --------------------------------------------------------------
-
-def compute_weights(points, centers):
-	weights = np.zeros(len(centers))
-	for point in points:
-		mycenter = 0
-		mindist = math.sqrt(point.squared_distance(centers[0]))
-		for i in range(1, len(centers)):
-			if math.sqrt(point.squared_distance(centers[i])) < mindist:
-				mindist = math.sqrt(point.squared_distance(centers[i]))
-				mycenter = i
-		weights[mycenter] = weights[mycenter] + 1
-	return weights
-
-
-def f2(k, L, iterations, partition):
-	points = [vector for vector in iter(partition)]
-	weights = np.ones(len(points))
-	centers = kmeansPP(points, weights, k, iterations)
-	final_weights = compute_weights(points, centers)
-	return [(vect, weight) for vect, weight in zip(centers, final_weights)]
-
-
-def MR_kmedian(pointset, k, L, iterations):
-	# ---------- ROUND 1 ---------------
-	coreset = pointset.mapPartitions(partial(f2, k, L, iterations))
-	# ---------- ROUND 2 ---------------
-	centersR1 = []
-	weightsR1 = []
-	for pair in coreset.collect():
-		centersR1.append(pair[0])
-		weightsR1.append(pair[1])
-	centers = kmeansPP(centersR1, weightsR1, k, iterations)
-
 
 # lloyd function
 # can take long time, e.g. 30 seconds for k=50, iter=3, |points|=10000
@@ -146,8 +114,44 @@ def kmeansPP(P, WP, k, iter):  # point_weights must be column vector
 	return new_centers
 
 
-# ---------- ROUND 3 --------------------------
-# ---------- ADD YOUR CODE HERE ---------------
+# ----------------------------------
+# ---- prepared code ---------------
+# ----------------------------------
+
+def compute_weights(points, centers):
+	weights = np.zeros(len(centers))
+	for point in points:
+		mycenter = 0
+		mindist = math.sqrt(point.squared_distance(centers[0]))
+		for i in range(1, len(centers)):
+			if math.sqrt(point.squared_distance(centers[i])) < mindist:
+				mindist = math.sqrt(point.squared_distance(centers[i]))
+				mycenter = i
+		weights[mycenter] = weights[mycenter] + 1
+	return weights
+
+
+def f2(k, L, iterations, partition):
+	points = [vector for vector in iter(partition)]
+	weights = np.ones(len(points))
+	centers = kmeansPP(points, weights, k, iterations)
+	final_weights = compute_weights(points, centers)
+	return [(vect, weight) for vect, weight in zip(centers, final_weights)]
+
+
+def MR_kmedian(pointset, k, L, iterations):
+	# ---------- ROUND 1 ---------------
+	coreset = pointset.mapPartitions(partial(f2, k, L, iterations))
+	# ---------- ROUND 2 ---------------
+	centersR1 = []
+	weightsR1 = []
+	for pair in coreset.collect():
+		centersR1.append(pair[0])
+		weightsR1.append(pair[1])
+	centers = kmeansPP(centersR1, weightsR1, k, iterations)
+
+	# ---------- ROUND 3 --------------------------
+	# ---------- ADD YOUR CODE HERE ---------------
 
 
 def f1(line):
